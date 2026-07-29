e749f5972e443cb901d0110c01aae1fb3647907a
Marty Pradere <martyp@labkey.com>
2026-07-29 12:18:06 -0600

    Add Estimated flag to conception; show offspring in ConceptionsByDam
    Offspring are aggregated per conception so multiple births for one conception don't duplicate rows.

--- nbri_ehr/resources/queries/nbri_ehr/ConceptionsByDam.sql
+++ nbri_ehr/resources/queries/nbri_ehr/ConceptionsByDam.sql
@@ -8,16 +8,18 @@ SELECT
     c.ConceptId,
     c.ConceptDate,
     c.ConceptTermDate,
+    c.Estimated,
     c.Sire,
     CASE
         WHEN b.conceptId IS NOT NULL THEN 'Live Birth'
         WHEN po.conceptId IS NOT NULL THEN COALESCE(po.result, 'Unknown')
         ELSE 'Unknown'
     END AS conceptionOutcome,
+    b.offspring,
     c.Remark,
     c.QCState AS qcstate
 FROM Conception c
-LEFT JOIN (SELECT DISTINCT b.conceptId FROM study.birth b WHERE b.conceptId IS NOT NULL) b
+LEFT JOIN (SELECT b.conceptId, GROUP_CONCAT(DISTINCT b.Id, ', ') AS offspring FROM study.birth b WHERE b.conceptId IS NOT NULL GROUP BY b.conceptId) b
     ON b.conceptId = c.ConceptId
 LEFT JOIN (SELECT p.conceptId, MAX(p.result.title) AS result FROM study.pregnancy p WHERE p.conceptId IS NOT NULL GROUP BY p.conceptId) po
     ON po.conceptId = c.ConceptId
