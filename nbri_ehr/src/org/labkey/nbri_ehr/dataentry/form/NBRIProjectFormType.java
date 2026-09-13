72fd8f85d4eaff0b886705e17a74eae9952e3d3c
Marty Pradere <martyp@labkey.com>
2026-09-13 09:36:26 -0600

    Restore the project, protocol and investigator admin form types
    These forms were dropped when the three lists moved to Manage Lookup Tables; both surfaces now edit them.

---
+++ nbri_ehr/src/org/labkey/nbri_ehr/dataentry/form/NBRIProjectFormType.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.labkey.nbri_ehr.dataentry.form;
+
+import org.labkey.api.ehr.dataentry.DataEntryFormContext;
+import org.labkey.api.ehr.dataentry.forms.AdminLinksFormType;
+import org.labkey.api.ehr.security.EHRProtocolEditPermission;
+import org.labkey.api.module.Module;
+import org.labkey.api.view.ActionURL;
+
+import java.util.ArrayList;
+
+public class NBRIProjectFormType extends AdminLinksFormType
+{
+    public NBRIProjectFormType(DataEntryFormContext ctx, Module owner)
+    {
+        super(ctx, owner, "Projects", "Projects", "Admin", new ArrayList<>());
+    }
+
+    @Override
+    protected ActionURL dataEntryLink()
+    {
+        ActionURL url = new ActionURL("ldk", "updateQuery", getCtx().getContainer());
+        url.addParameter("schemaName", "ehr");
+        url.addParameter("query.queryName", "project");
+        url.addParameter("showImport", "true");
+        return url;
+    }
+
+    @Override
+    public boolean isAvailable()
+    {
+        return (super.isAvailable() || getCtx().getContainer().hasPermission(getCtx().getUser(), EHRProtocolEditPermission.class));
+    }
+}
